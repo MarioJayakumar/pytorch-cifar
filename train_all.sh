4c5d3a00d0c6049c0b820edba6089086e2660642
Mario <mario.jayakumar@gmail.com>
2020-06-14 22:44:18 -0400

    mid training backup 1

--- train_all.sh
+++ train_all.sh
@@ -1,21 +1,12 @@
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model fc1 
 
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model fc2 
 
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model alexnet 
 
 CUDA_VISIBLE_DEVICES=1, python3 main.py --model GoogLeNet 
 
 CUDA_VISIBLE_DEVICES=1, python3 main.py --model lenet 
 
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet34 
 
 CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet50 
 
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet101 
-
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg11 
-
 CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg16 
 
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg19 
---
+++ train_all2.sh
@@ -0,0 +1,12 @@
+
+
+
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model resnet34 
+
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model resnet101 
+
+
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model vgg11 
+
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model vgg19 
+
