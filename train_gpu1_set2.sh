b187dd8a2cbf66fdf443de69244a4e9ff8ce676b
MarioJayakumar <mario.jayakumar@gmail.com>
2020-06-17 16:04:09 -0400

    another backup

--- train_gpu1_set2.sh
+++ train_gpu1_set2.sh
@@ -1,11 +1,11 @@
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg16  --run 0
+CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet101  --run 0
 
 
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg16  --run 1
+CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet101  --run 1
 
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg16 --run 2
+CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet101 --run 2
 
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg16 --run 3
+CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet101 --run 3
 
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg16 --run 4
+CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet101 --run 4
       
