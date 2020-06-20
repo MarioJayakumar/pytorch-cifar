0fa6ee1320b56a7506960d09eaa0262109eb2e6f
MarioJayakumar <mario.jayakumar@gmail.com>
2020-06-20 16:24:04 -0400

    resnet101 almost done

--- train_gpu0_set1.sh
+++ train_gpu0_set1.sh
@@ -1,6 +1,7 @@
 
-CUDA_VISIBLE_DEVICES=0, python3 main.py --model GoogLeNet  --run 0
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model resnet101  --run 0
 
 
-CUDA_VISIBLE_DEVICES=0, python3 main.py --model GoogLeNet  --run 1
-
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model resnet101  --run 1
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model resnet101  --run 2
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model resnet101  --run 3
--- train_gpu1_set1.sh
+++ train_gpu1_set1.sh
@@ -1,12 +1,3 @@
 
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg19 --run 1
-
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg19 --run 2
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg19 --run 3
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg19 --run 4
-
-
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet34 --run 1
-
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet34 --run 2
-CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet34 --run 3
+CUDA_VISIBLE_DEVICES=1, python3 main.py --model resnet50 --run 1
+CUDA_VISIBLE_DEVICES=1, python3 main.py --model vgg19 --run 0
