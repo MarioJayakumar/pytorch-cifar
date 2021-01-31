ce277c18fee40bd532ad3986c598e0a7305a669c
Mario Jayakumar <mario.jayakumar@gmail.com>
2021-01-31 16:14:56 -0500

    added vgg script

---
+++ statstop3_gpu0_secondary.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model vgg11 --run 75
+
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model vgg11 --run 76
+
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model vgg11 --run 77
+
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model vgg11 --run 78
+
+CUDA_VISIBLE_DEVICES=0, python3 main.py --model vgg11 --run 79
